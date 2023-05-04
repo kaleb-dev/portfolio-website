b0cd27f30af28385d7fad18600ba5e4db55a6c74
Sponge <kalebt567@gmail.com>
2023-05-04 09:18:10 -0400

    Resume complete (FIX BACKGROUND)

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,10 +1,11 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../Pages/Home/Home.scss";
 import Footer from "../../Components/Footer/Footer";
-import ResumePage from '../../Assets/Shopify+Resume+Tessema_Kaleb_Resume (1).pdf'
-import { Document, Page, pdfjs } from 'react-pdf';
-import './Resume.scss'
-
+import ResumePage from "../../Assets/Kaleb Tessema.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+import "./Resume.scss";
+import { Link } from "react-router-dom";
+import BgImage from '../../Assets/Images/resumeBg.png'
 
 function Resume() {
   const [numPages, setNumPages] = useState(null);
@@ -16,13 +17,10 @@ function Resume() {
 
   pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-
-
   return (
     <div id="landing">
-      {/* <Navbar homeNav="navbar navbar-expand-lg  bg-custom" /> */}
-      <div className="blueBlobMobile" />
-      <div className="purpBlobMobile" />
+      {/* <div className="blueBlobMobile" />
+      <div className="purpBlobMobile" /> */}
       <div
         className="landingContent"
         style={{
@@ -31,13 +29,19 @@ function Resume() {
           marginBottom: 50,
           display: "block",
           padding: 75,
+          backgroundImage: `url(${BgImage})`,
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover'
         }}
       >
         <h1>Resume</h1>
-        <div style={{padding: 50}}>
-        <Document file={ResumePage} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
+        <p style={{marginTop: 20, marginBottom: 20, maxWidth: 'unset'}}>
+          Feel free to reach out at my <Link className="underline" style={{fontWeight:700}} to="/contact">Contact Page</Link>. Also if you would like to <a href={ResumePage} className="underline"style={{fontWeight:700}} download='Kaleb_Tessema.pdf'>Download</a>
+        </p>
+        <div style={{ padding: 50 }}>
+          <Document file={ResumePage} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
         </div>
 
         <div className="blueBlob" />
